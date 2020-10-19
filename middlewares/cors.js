715ee405deae76f66cf56ad8275386d98693b34b
Maksim Shakhlin <maksim-shakhlin@yandex.ru>
2020-10-19 21:15:15 +0500

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,7 +5,14 @@ module.exports = (req, res, next) => {
   if (whitelist.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', 'content-type');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE',
+    );
   }
   if (req.method === 'OPTIONS') {
     res.status(204).end();
